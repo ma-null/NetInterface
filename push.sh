7e6d963abcb3c60129a88f00a4a4283fe211add2
ma-null <maypachulli@gmail.com>
2019-03-29 23:47:59 +0300

    add echo docker_password

--- push.sh
+++ push.sh
@@ -4,16 +4,8 @@ function tag_and_push {
         echo "Pushing docker image to hub tagged as $IMAGE_NAME:$1"
         docker build -t $IMAGE_NAME:$1 -t $IMAGE_NAME -f Dockerfile .
         docker push $IMAGE_NAME
-        docker push $IMAGE_NAME:$1
     fi
 }
 VERSION_TAG=v.$TRAVIS_BUILD_NUMBER
-cat > ~/.dockercfg << EOF
-{
-  "https://index.docker.io/v1/": {
-    "auth": "${HUB_AUTH}",
-    "email": "${HUB_EMAIL}"
-  }
-}
-EOF
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
     tag_and_push $VERSION_TAG
